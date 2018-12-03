ce374d5571c3c032a797955b2f8a8addfe125aa8
Aliaksei Kavalenka <Aliaksei_Kavalenka@epam.com>
2018-12-03 16:49:48 +0300

    new

---
+++ deploy_all.ps1
@@ -0,0 +1,25 @@
+
+write-host "Connect to your AzureRM account:"
+connect-azurermaccount
+
+$adminusername = read-host "Enter admin login"
+$keyvaultname = read-host "Enter vault name"
+write-host "Getting parameters for deployment . . . "
+$keyvaults = Get-AzureRmKeyVault
+foreach($keyvault in $keyvaults){
+    if($keyvault.vaultname -eq $keyvaultname){
+        $keyvaultresourceid = $keyvault.ResourceId
+    }
+    else {
+        write-host "There is no such vault in your account, reenter vault name"
+        $keyvaultname = read-host "Enter vault name"
+    }
+}
+$secretname = (Get-AzureKeyVaultSecret -VaultName $keyvaultname | where-object{$_.name -like "admin*"}).name
+$parameters = @(
+    $adminusername,
+    $keyvaultresourceid,
+    $secretname
+)
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName testrg -TemplateFile .\deployment.json -templateparameters $parameters
